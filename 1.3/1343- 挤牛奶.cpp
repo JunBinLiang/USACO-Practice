725376d060a4ed4d46cac55667e09490738a6d61
66brother <45537132+JunBinLiang@users.noreply.github.com>
2022-07-07 18:23:05 -0400

    Create 1343- 挤牛奶.cpp

---
+++ 1.3/1343- 挤牛奶.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <queue>
+#include <list>
+
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define ve vector
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define RFOR(i, a, b) for (int i = a; i >= b; i--)
+#define f first
+#define se second
+#define W while
+#define um unordered_map
+#define us unordered_set
+#define be begin
+#define en end
+
+// Some predefined templates
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const unordered_set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename T>
+ostream &operator<<(ostream &out, const set<T> &a)
+{
+  out << "[";
+  bool first = true;
+  for (auto v : a)
+  {
+    out << (first ? "" : ", ");
+    out << v;
+    first = 0;
+  }
+  out << "]";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+template <typename U, typename T, class Cmp>
+ostream &operator<<(ostream &out, const unordered_map<U, T, Cmp> &a)
+{
+  out << "{";
+  bool first = true;
+  for (auto &p : a)
+  {
+    out << (first ? "" : ", ");
+    out << p.first << ":" << p.second;
+    first = 0;
+  }
+  out << "}";
+  return out;
+}
+
+
+ll gcd(ll x, ll y)
+{
+  return (y == 0 ? x : gcd(y, x % y));
+}
+ll lcm(ll x, ll y)
+{
+  return x * y / gcd(x, y);
+}
+
+int to_int(string &s)
+{
+  int res = 0;
+  for (char c : s)
+  {
+    res = res * 10 + (c - '0');
+  }
+  return res;
+}
+
+string substring(string& s, int l, int r) {
+  string res = "";
+  for(int i = l; i <= r; i++) res += s[i];
+  return res;
+}
+
+/*class Compare
+{
+public:
+    bool operator() (pair<int, int>& a, pair<int, int>& b)
+    {
+        return a.first > b.first;
+    }
+    //a[0] > b[0] : min
+};*/
+
+
+struct Tuple {
+  int l, r, idx;
+};
+
+bool COMP(pair<int, int>& p1, pair<int, int>& p2) {
+  return p1.first < p2.first;
+}
+
+
+
+void solve() {
+  int n;
+  cin >> n;
+  ve<pair<int, int>> a;
+  for(int i = 0; i < n; i++) {
+    int l, r; cin >> l >> r;
+    a.pb({l, r});
+  }
+
+  sort(a.begin(), a.end(), COMP);
+  int res1 = 0, res2 = 0;
+  ve<pair<int, int>> sta;
+  for(pair<int, int>& p : a) {
+    if(sta.size() == 0) sta.pb(p);
+    else {
+      if(p.first > sta[sta.size() - 1].second) {
+        sta.pb(p);
+      } else {
+        sta[sta.size() - 1].second = max(sta[sta.size() - 1].second, p.second);
+      }
+    }
+  }
+
+  for(pair<int, int>& p : sta) {
+    res1 = max(res1, p.second - p.first);
+  }
+  
+  for(int i = 1; i < sta.size(); i++) {
+      res2 = max(res2, sta[i].first - sta[i - 1].second);
+  }
+
+  cout << res1 << " " << res2 << endl;
+}
+
+//区间连接问题
+int main()
+{
+
+  int t = 1;
+  //cin >> t;
+  W (t--)
+  { 
+    solve();
+  }
+  return 0;
+}
